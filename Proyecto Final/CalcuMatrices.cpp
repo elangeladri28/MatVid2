add659f47cd41f02fca85af9e431fee9c5342b23
elangeladri28 <carnallostes@gmail.com>
2019-04-01 10:59:35 -0600

    Calculadora (pequeño avance)

---
+++ Proyecto Final/CalcuMatrices.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int op;
+int Fila;
+int Col;
+float F1C11, F1C21, F1C31, F1C41, F1C51, F1C61, F1C71, F1C81, F1C91, F1C101;	//1 de la primera matriz
+float F2C11, F2C21, F2C31, F2C41, F2C51, F2C61, F2C71, F2C81, F2C91, F2C101;	//2 de la primera matriz
+float F3C11, F3C21, F3C31, F3C41, F3C51, F3C61, F3C71, F3C81, F3C91, F3C101;	//3 de la primera matriz
+float F4C11, F4C21, F4C31, F4C41, F4C51, F4C61, F4C71, F4C81, F4C91, F4C101;	//4 de la primera matriz
+float F5C11, F5C21, F5C31, F5C41, F5C51, F5C61, F5C71, F5C81, F5C91, F5C101;	//5 de la primera matriz
+float F6C11, F6C21, F6C31, F6C41, F6C51, F6C61, F6C71, F6C81, F6C91, F6C101;	//6 de la primera matriz
+float F7C11, F7C21, F7C31, F7C41, F7C51, F7C61, F7C71, F7C81, F7C91, F7C101;	//7 de la primera matriz
+float F8C11, F8C21, F8C31, F8C41, F8C51, F8C61, F8C71, F8C81, F8C91, F8C101;	//8 de la primera matriz
+float F9C11, F9C21, F9C31, F9C41, F9C51, F9C61, F9C71, F9C81, F9C91, F9C101;	//9 de la primera matriz
+float F10C11, F10C21, F10C31, F10C41, F10C51, F10C61, F10C71, F10C81, F10C91, F10C101;	//10 de la primera matriz
+
+float F1C12, F1C22, F1C32, F1C42, F1C52, F1C62, F1C72, F1C82, F1C92, F1C102;	//1 de la segunda matriz
+float F2C12, F2C22, F2C32, F2C42, F2C52, F2C62, F2C72, F2C82, F2C92, F2C102;	//2 de la segunda matriz
+float F3C12, F3C22, F3C32, F3C42, F3C52, F3C62, F3C72, F3C82, F3C92, F3C102;	//3 de la segunda matriz
+float F4C12, F4C22, F4C32, F4C42, F4C52, F4C62, F4C72, F4C82, F4C92, F4C102;	//4 de la segunda matriz
+float F5C12, F5C22, F5C32, F5C42, F5C52, F5C62, F5C72, F5C82, F5C92, F5C102;	//5 de la segunda matriz
+float F6C12, F6C22, F6C32, F6C42, F6C52, F6C62, F6C72, F6C82, F6C92, F6C102;	//6 de la segunda matriz
+float F7C12, F7C22, F7C32, F7C42, F7C52, F7C62, F7C72, F7C82, F7C92, F7C102;	//7 de la segunda matriz
+float F8C12, F8C22, F8C32, F8C42, F8C52, F8C62, F8C72, F8C82, F8C92, F8C102;	//8 de la segunda matriz
+float F9C12, F9C22, F9C32, F9C42, F9C52, F9C62, F9C72, F9C82, F9C92, F9C102;	//9 de la segunda matriz
+float F10C12, F10C22, F10C32, F10C42, F10C52, F10C62, F10C72, F10C82, F10C92, F10C102;	//10 de la segunda matriz
+
+void main() {
+	locale::global(locale("Spanish"));
+	cout << "¿Qué deseas hacer? \n 1.-Sumar\n 2.-Restar\n 3.-Multiplicar" << endl;
+	cin >> op;
+
+
+}
